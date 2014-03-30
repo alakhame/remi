42af26fb0983a149146340371e5f7fc204357d90
alakhame <ndiaye.khadim.inpt@gmail.com>
2014-03-30 22:38:18 +0200

    avec la base de données // khadim

--- Symfony/app/cache/dev/appDevDebugProjectContainer.php
+++ Symfony/app/cache/dev/appDevDebugProjectContainer.php
@@ -3423,7 +3423,7 @@ class appDevDebugProjectContainer extends Container
             'kernel.root_dir' => 'C:/Users/khadim/Desktop/wamp/www/remi/Symfony/app',
             'kernel.environment' => 'dev',
             'kernel.debug' => true,
-            'kernel.name' => 'ap_',
+            'kernel.name' => 'app',
             'kernel.cache_dir' => 'C:/Users/khadim/Desktop/wamp/www/remi/Symfony/app/cache/dev',
             'kernel.logs_dir' => 'C:/Users/khadim/Desktop/wamp/www/remi/Symfony/app/logs',
             'kernel.bundles' => array(
