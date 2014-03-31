d09ef7e43725171a7849f77f719b9a87106c6ea4
alakhame <ndiaye.khadim.inpt@gmail.com>
2014-03-31 19:04:34 +0000

    vue gnerale

--- Symfony/app/cache/dev/twig/b9/68/fba3243fc0273ee395ad89fbad7011258a7336b49da7ddabde2baf3f5bc8.php
+++ Symfony/app/cache/dev/twig/b9/68/fba3243fc0273ee395ad89fbad7011258a7336b49da7ddabde2baf3f5bc8.php
@@ -32,12 +32,14 @@ class __TwigTemplate_b968fba3243fc0273ee395ad89fbad7011258a7336b49da7ddabde2baf3
 \t<body class=\"\" style=\"\" onscroll=\"scroll();\" >
 \t 
         <div id=\"Entete_general\" style=\"\">
-\t\t\t\t
+\t\t\t\t<div id=\"imag_back_ground\" style=\"\">
+\t\t\t\t  
+\t     </div> 
 \t\t\t<div class=\"box \" style=\"\">
 \t\t\t
 \t\t\t\t <div id=\"logo_general\" style=\"\">
 \t\t\t\t    <img id=\"logo_image\" src=\"";
-        // line 17
+        // line 19
         echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("images/logo.png"), "html", null, true);
         echo "\" style=\"\"/>
 \t\t\t     </div> 
@@ -54,22 +56,22 @@ class __TwigTemplate_b968fba3243fc0273ee395ad89fbad7011258a7336b49da7ddabde2baf3
 \t\t\t<div id=\"menu\" style=\"\">
 <div id=\"menu_a\" style=\"\">\t
 \t<a id=\"menu_a_4\" href=\"";
-        // line 31
+        // line 33
         echo $this->env->getExtension('routing')->getPath("innovit_general_homepage", array("name" => "liensutiles"));
         echo "\"\"><div id=\"menu_4\" style=\"\" >مواقع مفيدة</div></a>
 \t\t
 \t<a id=\"menu_a_3\" href=\"";
-        // line 33
+        // line 35
         echo $this->env->getExtension('routing')->getPath("innovit_general_homepage", array("name" => "flashactus"));
         echo "\"\"><div id=\"menu_3\" style=\"\">مستجدات</div></a>
 \t
 \t<a id=\"menu_a_2\" href=\"";
-        // line 35
+        // line 37
         echo $this->env->getExtension('routing')->getPath("innovit_general_homepage", array("name" => "qsnous"));
         echo "\"\"><div id=\"menu_2\" style=\"\" >من نحن؟</div></a>
 \t
 \t<a id=\"menu_a_1\" href=\"";
-        // line 37
+        // line 39
         echo $this->env->getExtension('routing')->getPath("innovit_general_homepage", array("name" => "Accueil"));
         echo "\"\"><div id=\"menu_1\" style=\"\" >الرئسية</div></a>
 \t<div id=\"sing_in\" style=\"\" onclick=\"javascript:affichersingin()\" >تسجيل الدخول</div>
@@ -85,7 +87,7 @@ class __TwigTemplate_b968fba3243fc0273ee395ad89fbad7011258a7336b49da7ddabde2baf3
 \t\tتعريف
 \t\t<div id=\"div_fermer\"  style=\"\" onclick=\"javascript:fermersingin()\">
 \t    <img src=\"";
-        // line 50
+        // line 52
         echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("images/fermer.png"), "html", null, true);
         echo "\" style=\"height:100%;width:100%\"/>
 \t     </div>
@@ -115,21 +117,21 @@ class __TwigTemplate_b968fba3243fc0273ee395ad89fbad7011258a7336b49da7ddabde2baf3
 \t\t</div>       
 \t\t<div id=\"corp_general\" style=\"\"> 
 \t\t";
-        // line 77
+        // line 79
         $this->displayBlock('corp', $context, $blocks);
-        // line 80
+        // line 82
         echo "\t\t</div>
 \t\t<div id=\"footer\" style=\"\">
 \t\t<div id=\"images_footer\">
 \t\t<div id=\"innovit\" class=\"footer_image\" style=\"\">
 \t\t<img id=\"logo_image\" src=\"";
-        // line 84
+        // line 86
         echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("images/innovIT_logo.png"), "html", null, true);
         echo "\" style=\"width:100%\"/>
         </div>
         <div id=\"inpt\" class=\"footer_image\" style=\"\">
 \t\t<img id=\"logo_image\" src=\"";
-        // line 87
+        // line 89
         echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("images/logoINPT.png"), "html", null, true);
         echo "\" style=\"width:100%\"/>
         </div>
@@ -143,7 +145,7 @@ class __TwigTemplate_b968fba3243fc0273ee395ad89fbad7011258a7336b49da7ddabde2baf3
 \t\t
         </div>
 \t<script  src=\"";
-        // line 98
+        // line 100
         echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("scripts/script_accueil.js"), "html", null, true);
         echo "\" > </script>
 \t\t\t\t
@@ -151,10 +153,10 @@ class __TwigTemplate_b968fba3243fc0273ee395ad89fbad7011258a7336b49da7ddabde2baf3
 </html>";
     }
 
-    // line 77
+    // line 79
     public function block_corp($context, array $blocks = array())
     {
-        // line 78
+        // line 80
         echo "\t\t
 \t\t";
     }
@@ -171,6 +173,6 @@ class __TwigTemplate_b968fba3243fc0273ee395ad89fbad7011258a7336b49da7ddabde2baf3
 
     public function getDebugInfo()
     {
-        return array (  155 => 77,  127 => 84,  58 => 31,  20 => 1,  76 => 4,  81 => 42,  480 => 162,  474 => 161,  469 => 158,  461 => 155,  457 => 153,  453 => 151,  444 => 149,  440 => 148,  437 => 147,  435 => 146,  430 => 144,  427 => 143,  423 => 142,  413 => 134,  409 => 132,  407 => 131,  402 => 130,  398 => 129,  393 => 126,  387 => 122,  384 => 121,  381 => 120,  379 => 119,  374 => 116,  368 => 112,  365 => 111,  362 => 110,  360 => 109,  355 => 106,  341 => 105,  337 => 103,  322 => 101,  314 => 99,  312 => 98,  309 => 97,  305 => 95,  298 => 91,  294 => 90,  285 => 89,  283 => 88,  278 => 86,  268 => 85,  264 => 84,  258 => 81,  252 => 80,  247 => 78,  241 => 77,  229 => 73,  220 => 70,  214 => 69,  177 => 65,  169 => 60,  140 => 55,  132 => 51,  128 => 49,  107 => 62,  61 => 13,  273 => 96,  269 => 94,  254 => 92,  243 => 88,  240 => 86,  238 => 85,  235 => 74,  230 => 82,  227 => 81,  224 => 71,  221 => 77,  219 => 76,  217 => 75,  208 => 68,  204 => 72,  179 => 69,  159 => 61,  143 => 56,  135 => 53,  119 => 77,  102 => 32,  71 => 19,  67 => 34,  63 => 33,  59 => 28,  38 => 6,  94 => 52,  89 => 50,  85 => 25,  75 => 17,  68 => 35,  56 => 9,  87 => 25,  21 => 2,  26 => 6,  93 => 28,  88 => 6,  78 => 21,  46 => 21,  27 => 4,  44 => 12,  31 => 3,  28 => 7,  201 => 92,  196 => 90,  183 => 82,  171 => 61,  166 => 71,  163 => 62,  158 => 78,  156 => 66,  151 => 63,  142 => 59,  138 => 54,  136 => 56,  121 => 80,  117 => 44,  105 => 40,  91 => 27,  62 => 23,  49 => 19,  24 => 4,  25 => 3,  19 => 2,  79 => 5,  72 => 31,  69 => 25,  47 => 9,  40 => 18,  37 => 7,  22 => 2,  246 => 90,  157 => 56,  145 => 46,  139 => 45,  131 => 52,  123 => 47,  120 => 40,  115 => 43,  111 => 37,  108 => 36,  101 => 32,  98 => 31,  96 => 31,  83 => 35,  74 => 38,  66 => 15,  55 => 15,  52 => 21,  50 => 23,  43 => 8,  41 => 17,  35 => 4,  32 => 3,  29 => 2,  209 => 82,  203 => 78,  199 => 67,  193 => 73,  189 => 71,  187 => 84,  182 => 66,  176 => 64,  173 => 65,  168 => 72,  164 => 59,  162 => 57,  154 => 58,  149 => 51,  147 => 98,  144 => 49,  141 => 48,  133 => 87,  130 => 41,  125 => 44,  122 => 43,  116 => 41,  112 => 42,  109 => 34,  106 => 36,  103 => 32,  99 => 31,  95 => 28,  92 => 21,  86 => 28,  82 => 22,  80 => 19,  73 => 37,  64 => 29,  60 => 30,  57 => 11,  54 => 25,  51 => 14,  48 => 13,  45 => 17,  42 => 19,  39 => 9,  36 => 5,  33 => 4,  30 => 7,);
+        return array (  160 => 80,  129 => 86,  70 => 37,  65 => 35,  20 => 1,  76 => 4,  81 => 42,  480 => 162,  474 => 161,  469 => 158,  461 => 155,  457 => 153,  453 => 151,  444 => 149,  440 => 148,  437 => 147,  435 => 146,  430 => 144,  427 => 143,  423 => 142,  413 => 134,  409 => 132,  407 => 131,  402 => 130,  398 => 129,  393 => 126,  387 => 122,  384 => 121,  381 => 120,  379 => 119,  374 => 116,  368 => 112,  365 => 111,  362 => 110,  360 => 109,  355 => 106,  341 => 105,  337 => 103,  322 => 101,  314 => 99,  312 => 98,  309 => 97,  305 => 95,  298 => 91,  294 => 90,  285 => 89,  283 => 88,  278 => 86,  268 => 85,  264 => 84,  258 => 81,  252 => 80,  247 => 78,  241 => 77,  229 => 73,  220 => 70,  214 => 69,  177 => 65,  169 => 60,  140 => 55,  132 => 51,  128 => 49,  107 => 62,  61 => 13,  273 => 96,  269 => 94,  254 => 92,  243 => 88,  240 => 86,  238 => 85,  235 => 74,  230 => 82,  227 => 81,  224 => 71,  221 => 77,  219 => 76,  217 => 75,  208 => 68,  204 => 72,  179 => 69,  159 => 61,  143 => 56,  135 => 89,  119 => 42,  102 => 32,  71 => 19,  67 => 34,  63 => 15,  59 => 28,  38 => 6,  94 => 52,  89 => 20,  85 => 25,  75 => 39,  68 => 14,  56 => 9,  87 => 25,  21 => 2,  26 => 6,  93 => 28,  88 => 6,  78 => 21,  46 => 21,  27 => 4,  44 => 12,  31 => 3,  28 => 7,  201 => 92,  196 => 90,  183 => 82,  171 => 61,  166 => 71,  163 => 62,  158 => 67,  156 => 66,  151 => 63,  142 => 59,  138 => 54,  136 => 56,  121 => 79,  117 => 44,  105 => 40,  91 => 52,  62 => 23,  49 => 19,  24 => 4,  25 => 3,  19 => 2,  79 => 5,  72 => 31,  69 => 25,  47 => 9,  40 => 18,  37 => 7,  22 => 2,  246 => 90,  157 => 79,  145 => 46,  139 => 45,  131 => 52,  123 => 82,  120 => 40,  115 => 43,  111 => 37,  108 => 36,  101 => 32,  98 => 31,  96 => 31,  83 => 35,  74 => 38,  66 => 15,  55 => 15,  52 => 21,  50 => 23,  43 => 19,  41 => 7,  35 => 4,  32 => 3,  29 => 2,  209 => 82,  203 => 78,  199 => 67,  193 => 73,  189 => 71,  187 => 84,  182 => 66,  176 => 64,  173 => 65,  168 => 72,  164 => 59,  162 => 57,  154 => 58,  149 => 100,  147 => 58,  144 => 49,  141 => 48,  133 => 55,  130 => 41,  125 => 44,  122 => 43,  116 => 41,  112 => 42,  109 => 34,  106 => 36,  103 => 32,  99 => 31,  95 => 28,  92 => 21,  86 => 28,  82 => 22,  80 => 19,  73 => 19,  64 => 29,  60 => 33,  57 => 11,  54 => 25,  51 => 14,  48 => 13,  45 => 17,  42 => 19,  39 => 9,  36 => 5,  33 => 4,  30 => 7,);
     }
 }
